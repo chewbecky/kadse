de9f13fdb92ba0285f3b5c7de4cfe2a8cd54a527
chewbecca <beckybettinger@yahoo.de>
2020-12-26 18:41:03 +0100

    🔧 service worker
    add service worker

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import theme from "./theme";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./assets/LCARSGTJ3.ttf";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
@@ -13,4 +14,14 @@ ReactDOM.render(
   </ChakraProvider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
